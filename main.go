a359d147cbb10708182ba8605dd4ca7830e11e0f
NEERAJAGADDAM <211230020@nitdelhi.ac.in>
2025-02-28 22:39:16 +0530

    main commit

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import (
 <<<<<<< HEAD
 	"Inventory-Management/inventory"
@@ -117,3 +118,32 @@ func main() {
 	h.Sleep() 
 }
 >>>>>>> dcbabf7 (fourth commit)
+=======
+
+
+import "fmt"
+
+
+
+func main() {
+
+    s1 := []int{1, 2, 3, 4}
+
+    s2 := make([]int, len(s1))
+
+    copy(s2, s1)
+
+
+
+    s1[0] = 100
+
+    fmt.Println(s1, s2)
+
+
+
+    s1 = append(s1, 200)
+
+    fmt.Println(s1, s2)
+
+}
+>>>>>>> 39b5552 (main commit)
